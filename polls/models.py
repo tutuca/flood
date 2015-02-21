9ff98ebd1451b4e9907ec301d78425ff07aa80c6
Matías Iturburu <maturburu@gmail.com>
2015-02-21 12:17:59 -0300

    Modelos compatibles con python, agrego campo mobiliario_afectado.

--- polls/models.py
+++ polls/models.py
@@ -1,21 +1,24 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class EstadoVivienda(models.Model):
     estado = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.estado
 
-
+@python_2_unicode_compatible
 class TipoMejora(models.Model):
     mejora = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.mejora
 
 
-# Create your models here.
+@python_2_unicode_compatible
 class Vivienda(models.Model):
     autor = models.ForeignKey('auth.User', editable=False)
     familia = models.CharField(max_length=255)
@@ -26,6 +29,7 @@ class Vivienda(models.Model):
     recurrencia_inundacion = models.CharField(max_length=255)
     notas = models.TextField(default='')
     estado_vivienda = models.ForeignKey(EstadoVivienda)
+    mobiliario_afectado = models.BooleanField(default=False)
     mejora_necesaria = models.ManyToManyField(TipoMejora)
     alta = models.DateTimeField(auto_now_add=True, editable=False)
 
@@ -33,10 +37,10 @@ class Vivienda(models.Model):
     def cantidad_personas(self):
         return self.persona_set.all().count()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.familia
 
-
+@python_2_unicode_compatible
 class Persona(models.Model):
     vivienda = models.ForeignKey(Vivienda)
     nombre = models.CharField(max_length=255)
@@ -50,10 +54,10 @@ class Persona(models.Model):
     herramientas = models.TextField(default='')
     notas = models.TextField(default='')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.nombre
 
-
+@python_2_unicode_compatible
 class TipoVivienda(models.Model):
     vivienda = models.ForeignKey(Vivienda)
     piso = models.CharField(max_length=255)
@@ -61,5 +65,5 @@ class TipoVivienda(models.Model):
     techo = models.CharField(max_length=255)
     instalaciones = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return ''.join([self.piso, self.paredes, self.techo, self.instalaciones])
